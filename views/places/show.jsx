5733e729487bea94679226d13b70f3b99617e1d3
Vinh Pham <phamv1@unlv.nevada.edu>
2022-01-19 19:48:23 -0800

    Delete functionality implemented

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,40 @@
 const React = require('react');
 const Def = require('../default');
 
-function show () {
+function show (data, id) {
     return (
         <Def>
-            <main>
-                <h1>Show Page</h1>
+            <main className='container p-5' >
+                <div className='row'>
+                    <div className='col'>
+                        <img width={'400em'} height={'400em'} src={'../'+data.place.pic} alt={ data.place.name}></img>
+                    </div>
+                    <div className='col'>
+                        <h1>{ data.place.name }</h1>
+                        <br></br>
+                        <h3>Rating</h3>
+                        <p>Not Rated</p>
+                        <h3>Description</h3>
+                        <p>{ `Locate in ${data.place.city}, ${data.place.state} and serving ${data.place.cusines}`}</p>
+                    </div>
+                    
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <h3>Comments</h3>
+                        <p>No Comments yet!</p>
+                    </div>
+                </div>
+                <div className='row'>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                    Edit
+                </a>     
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form> 
+                </div>
             </main>
         </Def>
     )
